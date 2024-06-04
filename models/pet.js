74374c96c59ba10c68d6bed021f5cd65a3c6d221
DominicBarnett <dominic.barnett@icloud.com>
2024-06-04 12:20:16 -0700

    Code to test with Dani

--- models/pet.js
+++ models/pet.js
@@ -1,23 +1,26 @@
 "use strict";
 
-const mongoose = require('mongoose');
+const mongoose = require('mongoose'),
+        Schema = mongoose.Schema;
 const mongoosePaginate = require('mongoose-paginate');
-const Schema = mongoose.Schema;
+
+mongoosePaginate.paginate.options = {
+  limit: 3 // how many records on each page
+};
 
 const PetSchema = new Schema({
-    name            : { type: String, required: true }
-  , species         : { type: String }
-  , birthday        : { type: Date }
-  , picUrl          : { type: String }
-  , picUrlSq        : { type: String }
-  , favoriteFood    : { type: String }
-  , description     : { type: String }
-},
-{
+  name: { type: String, required: true }
+  , birthday: {type: String, required: true }
+  , species: { type: String, required: true }
+  , picUrl: { type: String }
+  , picUrlSq: { type: String }
+  , avatarUrl: { type: String, required: true }
+  , favoriteFood: { type: String, required: true }
+  , description: { type: String, minlength: 10, required: true }
+}, {
   timestamps: true
 });
 
-mongoosePaginate.paginate.options = { limit: 3 };
 PetSchema.plugin(mongoosePaginate);
 
 module.exports = mongoose.model('Pet', PetSchema);
